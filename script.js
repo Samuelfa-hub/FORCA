965686fb1e845c84fb1680e44df48d108ba3ece5
Samuelfa <samuelfaalmeida2008@gmail.com>
2025-06-25 14:49:19 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const SumbmitBtn = document.getElementById("MySubmit"); 
+const Input = document.getElementById("meuInput"); 
+const WordDisplay = document.getElementById("word-display"); 
+const Letters = [
+  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+  "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+];
+
+
+
+
+SumbmitBtn.addEventListener("click",play());
