e2979ae27c20eacab3abb2ffd5d6b9a02f80417f
Samuelfa <samuelfaalmeida2008@gmail.com>
2025-06-30 15:53:01 -0300

    finalizando todas as funções necessarias para o jogo funcionar.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 //Está parte inicial gera a palavra aleatoria assim que a janela é aberta (Incrementar um botão para gerar uma nova palvra sem ter q dar reload ou ganhar/perder).
-const starWarsElementos = [
+const starWarsElementos = [//*Criar um Array de palavras para ser sorteado. 
   "Luke Skywalker",
   "Darth Vader",
   "Princesa Leia Organa",
@@ -32,20 +32,22 @@ const starWarsElementos = [
   "Tatooine",
   "Coruscant"
 ];
-let PalavraEscolhida = starWarsElementos[Math.floor(Math.random() * starWarsElementos.length)] 
-let CrudeWord = PalavraEscolhida.replaceAll(" ","").replaceAll("-","").toUpperCase()// simplificando a palavra para lançala para o computador ler de forma crua.
 
-console.log(CrudeWord)
+let PalavraEscolhida = starWarsElementos[Math.floor(Math.random() * starWarsElementos.length)]
+PalavraEscolhida = PalavraEscolhida.toUpperCase().replaceAll("-",""); 
+let PalavraCrua = PalavraEscolhida.replaceAll(" ","").toUpperCase()// simplificando a palavra para lançala para o computador ler de forma crua.
+
+console.log(PalavraCrua)
 //*Fazer um filtro de caracteres que so vai permitir que letras de A a Z passem
 function LetrasPermitidas() { //*Fazer um filtro de caracteres que so vai permitir que letras de A a Z passem
     //*transformar letras Minusculas em Maiusculas 
-    let ChosedLetter = Input.value.trim(); 
-    ChosedLetter = ChosedLetter.toUpperCase();
-    console.log(ChosedLetter);
+    let LetraEscolhida = Input.value.trim(); 
+    LetraEscolhida = LetraEscolhida.toUpperCase();
+    console.log(LetraEscolhida);
     //Fazendo a verificação se a letra é permitida, ou seja se ela bate com o array de letras. 
-    if (Letters.includes(ChosedLetter)) { //É uma letra permitida
+    if (Letters.includes(LetraEscolhida)) { //É uma letra permitida
         console.log("deu boa é permitida")
-        return ChosedLetter
+        return LetraEscolhida
     }
     else { //Não é uma letra permitida
         console.log("Foi mal irmão coloca uma letra ai!")
@@ -60,22 +62,40 @@ function VidasDecreaser () {
   vidas = vidas - 1;// backend 
   LifesCounter.innerHTML = LifesCounter.innerHTML -1 //frontend
   DisplayLifes.src = VidasImg[vidas-1];//trocando as sprites de vida 
-
+  IDspartes[vidas].style.visibility = "visible";//*Mudar o Sprite do Bonequinho conforme a quantidade de vidas  
   if(vidas === 0) {alert("Você perdeu!")} // criar uma função de game over
-  console.log(vidas); //flag
 }
+function Play () {//função responsavel pelo jogo 
+  //verificando se a letra escolhida e permitida
+  let LetraEscolhida = LetrasPermitidas();
+  
+    if(PalavraCrua.includes(LetraEscolhida))//*Checar se a palavra escondida contem a letra sorteada  
+    {
+      Trocar(LetraEscolhida)
+    }
+    else{//jogador erra a letra
+    console.log("não contem a letra")
+      VidasDecreaser();
+      LetrasErradas.innerHTML =  LetraEscolhida + "," + LetrasErradas.innerHTML ; 
+    }
+   
+};
 const Letters = [//Array letras
   "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-  "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+  "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","1","2","3","8"
 ];
-  //primeira parte
-  //preparando a palavra para mostrala
-  var ArrayPe = PalavraEscolhida.split("");//para o player
-  var ArrayCW = CrudeWord.split("");//para o sistema
-  for(i = 0;i < PalavraEscolhida.length; i++) {
-    UserDisplay.push("_");
+//está função coloca a letra escolhida em seu devido lugar 
+function Trocar (LetraEscolhida) //*Mudar do caracter secreto para a letra escolhida se certa (na posição certa)
+{ 
+  for(let i = 0; i < UserDisplay.length;i++)
+  {
+    if (ArrayCW[i] == LetraEscolhida)
+    {
+      UserDisplay[i] = LetraEscolhida;
+      WordDisplay.innerHTML = UserDisplay; //Mostrando a palavra na tela 
+    }
   }
-  WordDisplay.innerHTML = UserDisplay;
+}
 
 //var vidas: 
   let vidas = 6;
@@ -89,6 +109,7 @@ const WrongLetters = document.getElementById("wrong-letters");
 const LifesCounter = document.getElementById("lives-count");
 const DisplayLifes = document.getElementById("Hearts");
 const Partes = document.getElementById("Parte1");
+const LetrasErradas = document.getElementById("wrong-letters");
 const VidasImg = [
 "./Assets/1Hearts.png",
 "./Assets/2Hearts.png",
@@ -97,47 +118,21 @@ const VidasImg = [
 "./Assets/5Hearts.png",
 "./Assets/6Hearts.png"
 ]
-
-
-
-
-// ficar sempre antes da função Escolher palavras acontecer 
-
-//Mostrando a palavra na tela 
-//*Mudar o Sprite do Bonequinho conforme a quantidade de vidas  
-//*Checar se a palavra escondida contem a letra sorteada 
-//*Mudar do caracter secreto para a letra escolhida se certa (na posição certa)
-//*Criar um Array de palavras para ser sorteado. 
-//*retirar todos os espaços e caracteres especiais antes de mandar para a palavra secreta para que eles não nececitem ser digitados 
-//*
-
-
-function Trocar () 
-{ 
-  for(let i = 0; i < CrudeWord.length;i++)
-  {
-    if (ArrayPe[i] == ChosedLetter)
-    {
-      UserDisplay[i] = ChosedLetter; 
-    }
+const IDspartes = [ 
+  document.getElementById("Perna2"),
+  document.getElementById("Perna1"),
+  document.getElementById("Braco2"),
+  document.getElementById("Braco1"),
+  document.getElementById("Tronco"),
+  document.getElementById("Cabeca"),
+]
+ //primeira parte
+  //preparando a palavra para mostrala
+  var ArrayPe = PalavraEscolhida.split("");//para o player
+  var ArrayCW = PalavraCrua.split("");//para o sistema
+  for(i = 0;i < PalavraEscolhida.length; i++) {
+    UserDisplay.push("_");
   }
-}
+  WordDisplay.innerHTML = UserDisplay;
 
-
-
-function Play () { 
-  //verificando se a letra escolhida e permitida
-  let ChosedLetter = LetrasPermitidas(); 
-  
-    if(CrudeWord.includes(ChosedLetter))//jogador acerta a letra e troca na forca se aproximando da vitoria. 
-    {
-      Trocar()
-    }
-    else{
-    console.log("não contem a letra")
-      VidasDecreaser();
-    
-    }
-   
-};
 SumbmitBtn.addEventListener("click",Play);
